22ca6ca0aeee26eecadde734026d29bb70c209c6
Dominic <dominiclupo5318@gmail.com>
2022-12-11 08:24:28 -0500

    Add day eight part two

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs};
+use std::{cmp::max, collections::HashMap, fs};
 
 const FILE_NAME: &str = "data1.txt";
 const CHAR_BYTE_TO_NUMBER: usize = 48;
@@ -8,6 +8,7 @@ fn main() {
     data.pop();
 
     part_one(&data);
+    part_two(&data);
 }
 
 fn part_one(data: &str) {
@@ -21,6 +22,17 @@ fn part_one(data: &str) {
     });
 
     println!("Part one: {}", visible_tree_count);
+}
+
+fn part_two(data: &str) {
+    let trees = Trees::new(data);
+
+    let mut max_visiblity = 0;
+    trees.heights.keys().for_each(|point| {
+        max_visiblity = max(max_visiblity, trees.get_visibility(point));
+    });
+
+    println!("Part two: {}", max_visiblity);
 }
 
 #[derive(Eq, Hash, PartialEq)]
@@ -60,6 +72,13 @@ impl Trees {
             || self.is_bottom_visible(point)
     }
 
+    fn get_visibility(&self, point: &Point) -> usize {
+        self.get_top_visibility(point)
+            * self.get_left_visibility(point)
+            * self.get_right_visibility(point)
+            * self.get_bottom_visibility(point)
+    }
+
     fn is_left_visible(&self, point: &Point) -> bool {
         let y = point.1;
         let height = self.heights.get(point).unwrap();
@@ -76,6 +95,28 @@ impl Trees {
             }
         }
         true
+    }
+
+    fn get_left_visibility(&self, point: &Point) -> usize {
+        let y = point.1;
+        let height = self.heights.get(point).unwrap();
+        let mut visibility = 0;
+        for i in (0..point.0).rev() {
+            let left_tree_height = match self.heights.get(&Point(i, y)) {
+                Some(height) => height,
+                None => panic!("Tree should exist"),
+            };
+
+            visibility += 1;
+
+            // tree is not visible from left since at least one tree
+            // to the left of it is taller or same height
+            if left_tree_height >= height {
+                return visibility;
+            }
+        }
+
+        visibility
     }
 
     fn is_right_visible(&self, point: &Point) -> bool {
@@ -96,6 +137,27 @@ impl Trees {
         true
     }
 
+    fn get_right_visibility(&self, point: &Point) -> usize {
+        let y = point.1;
+        let height = self.heights.get(point).unwrap();
+        let mut visibility = 0;
+        for i in point.0 + 1..=self.max_x_index {
+            let right_tree_height = match self.heights.get(&Point(i, y)) {
+                Some(height) => height,
+                None => panic!("Tree should exist"),
+            };
+
+            visibility += 1;
+
+            // tree is not visible from right since at least one tree
+            // to the right of it is taller or same height
+            if right_tree_height >= height {
+                return visibility;
+            }
+        }
+        visibility
+    }
+
     fn is_top_visible(&self, point: &Point) -> bool {
         let x = point.0;
         let height = self.heights.get(point).unwrap();
@@ -114,6 +176,27 @@ impl Trees {
         true
     }
 
+    fn get_top_visibility(&self, point: &Point) -> usize {
+        let x = point.0;
+        let height = self.heights.get(point).unwrap();
+        let mut visibility = 0;
+        for i in (0..point.1).rev() {
+            let top_tree_height = match self.heights.get(&Point(x, i)) {
+                Some(height) => height,
+                None => panic!("Tree should exist"),
+            };
+
+            visibility += 1;
+
+            // tree is not visible from top since at least one tree
+            // to the top of it is taller or same height
+            if top_tree_height >= height {
+                return visibility;
+            }
+        }
+        visibility
+    }
+
     fn is_bottom_visible(&self, point: &Point) -> bool {
         let x = point.0;
         let height = self.heights.get(point).unwrap();
@@ -130,6 +213,27 @@ impl Trees {
             }
         }
         true
+    }
+
+    fn get_bottom_visibility(&self, point: &Point) -> usize {
+        let x = point.0;
+        let height = self.heights.get(point).unwrap();
+        let mut visibility = 0;
+        for i in point.1 + 1..=self.max_y_index {
+            let top_tree_height = match self.heights.get(&Point(x, i)) {
+                Some(height) => height,
+                None => panic!("Tree should exist"),
+            };
+
+            visibility += 1;
+
+            // tree is not visible from top since at least one tree
+            // to the top of it is taller or same height
+            if top_tree_height >= height {
+                return visibility;
+            }
+        }
+        visibility
     }
 }
 
@@ -214,5 +318,74 @@ mod tests {
         assert_eq!(trees.is_visible(&Point(1, 2)), false);
         assert_eq!(trees.is_visible(&Point(1, 3)), true);
         assert_eq!(trees.is_visible(&Point(1, 3)), true);
+    }
+
+    #[test]
+    fn test_trees_visibility() {
+        /*
+        111111
+        121122
+        111111
+        */
+        let data = "111111\n121122\n111111\n";
+
+        let trees = Trees::new(data);
+        assert_eq!(trees.get_visibility(&Point(0, 1)), 0);
+        assert_eq!(trees.get_visibility(&Point(1, 1)), 3);
+        assert_eq!(trees.get_visibility(&Point(2, 1)), 1);
+        assert_eq!(trees.get_visibility(&Point(3, 1)), 1);
+        assert_eq!(trees.get_visibility(&Point(4, 1)), 3);
+        assert_eq!(trees.get_visibility(&Point(5, 1)), 0);
+
+        /*
+        111111
+        221121
+        111111
+        */
+        let data = "111111\n221121\n111111\n";
+
+        let trees = Trees::new(data);
+        assert_eq!(trees.get_visibility(&Point(0, 1)), 0);
+        assert_eq!(trees.get_visibility(&Point(1, 1)), 3);
+        assert_eq!(trees.get_visibility(&Point(2, 1)), 1);
+        assert_eq!(trees.get_visibility(&Point(3, 1)), 1);
+        assert_eq!(trees.get_visibility(&Point(4, 1)), 3);
+        assert_eq!(trees.get_visibility(&Point(5, 1)), 0);
+
+        /*
+        111
+        121
+        111
+        111
+        121
+        121
+        */
+        let data = "111\n121\n111\n111\n121\n121\n";
+
+        let trees = Trees::new(data);
+        assert_eq!(trees.get_visibility(&Point(1, 0)), 0);
+        assert_eq!(trees.get_visibility(&Point(1, 1)), 3);
+        assert_eq!(trees.get_visibility(&Point(1, 2)), 1);
+        assert_eq!(trees.get_visibility(&Point(1, 3)), 1);
+        assert_eq!(trees.get_visibility(&Point(1, 4)), 3);
+        assert_eq!(trees.get_visibility(&Point(1, 5)), 0);
+
+        /*
+        121
+        121
+        111
+        111
+        121
+        111
+        */
+        let data = "121\n121\n111\n111\n121\n111\n";
+
+        let trees = Trees::new(data);
+        assert_eq!(trees.get_visibility(&Point(1, 0)), 0);
+        assert_eq!(trees.get_visibility(&Point(1, 1)), 3);
+        assert_eq!(trees.get_visibility(&Point(1, 2)), 1);
+        assert_eq!(trees.get_visibility(&Point(1, 3)), 1);
+        assert_eq!(trees.get_visibility(&Point(1, 4)), 3);
+        assert_eq!(trees.get_visibility(&Point(1, 5)), 0);
     }
 }
