b290ca4042dd7cd95f4c38ee277f506a3db0e562
Dominic <dominiclupo5318@gmail.com>
2022-12-22 23:51:20 -0500

    Add day three part two

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,12 +1,17 @@
-use std::{collections::HashSet, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 const FILE_NAME: &str = "data1.txt";
+const PART_TWO_SACK_COUNT: usize = 2;
 
 fn main() {
     let mut data = fs::read_to_string(FILE_NAME).expect("Something went wrong reading the file");
     data.pop();
 
     part_one(&data);
+    part_two(&data);
 }
 
 fn part_one(data: &str) {
@@ -26,6 +31,55 @@ fn part_one(data: &str) {
     });
 
     println!("Part one: {}", total);
+}
+
+fn part_two(data: &str) {
+    let mut total = 0;
+    let mut first_sack: HashSet<char> = HashSet::new();
+    let mut second_sack: HashSet<char> = HashSet::new();
+    let mut third_sack: HashSet<char> = HashSet::new();
+    let mut char_counts: HashMap<char, usize> = HashMap::new();
+
+    data.lines().for_each(|line| {
+        let chars = line.chars().collect::<Vec<_>>();
+        if first_sack.is_empty() {
+            first_sack = HashSet::from_iter(chars.into_iter());
+        } else if second_sack.is_empty() {
+            second_sack = HashSet::from_iter(chars.into_iter());
+        } else if third_sack.is_empty() {
+            third_sack = HashSet::from_iter(chars.into_iter());
+
+            for c in &first_sack {
+                count_chars(*c, &mut char_counts)
+            }
+            for c in &second_sack {
+                count_chars(*c, &mut char_counts)
+            }
+            for c in &third_sack {
+                count_chars(*c, &mut char_counts)
+            }
+
+            for (c, count) in &char_counts {
+                if count == &PART_TWO_SACK_COUNT {
+                    total += get_priority(*c);
+                }
+            }
+
+            first_sack.clear();
+            second_sack.clear();
+            third_sack.clear();
+            char_counts.clear();
+        }
+    });
+
+    println!("Part two: {}", total);
+}
+
+fn count_chars(c: char, char_counts: &mut HashMap<char, usize>) {
+    match char_counts.get(&c) {
+        Some(count) => char_counts.insert(c, count + 1),
+        None => char_counts.insert(c, 1),
+    };
 }
 
 fn get_priority(c: char) -> u32 {
